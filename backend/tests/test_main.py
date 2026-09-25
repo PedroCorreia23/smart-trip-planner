b35a0894c28dc85be1121e6f1435d3499d9c1300
zini23 <pedro.correia.piruzzo@gmail.com>
2026-09-25 20:04:19 +0100

    test: add mocks to search trip endpoint

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -19,17 +19,28 @@ def test_search_trip_valid():
         "end_date": "2026-10-15"
     }
 
+    fake_coords = {"lat": 48.85, "lon": 2.35}
+
+    fake_weather = {
+        "time": ["2026-09-25", "2026-09-26"],
+        "temperature_2m_max": [25.0, 22.0],
+        "temperature_2m_min": [15.0, 14.0],
+        "precipitation_sum": [0.0, 1.2]
+    }
+
     # 2. Intercetamos o get_coordinates para ele não ir à Internet
-    with patch("app.main.GeocodingService.get_coordinates", new_callable=AsyncMock) as mock_geo:
-        # Definimos as coordenadas falsas que o serviço deve devolver instantaneamente
-        mock_geo.return_value = {"lat": 48.8534, "lon": 2.3483}
+    with patch("app.main.GeocodingService.get_coordinates", return_value=fake_coords),\
+         patch("app.main.WeatherService.get_weather", return_value=fake_weather):
         
-        # 3. Fazemos o pedido com o payload
-        response = client.post("/trips/search", json=payload)
+            # 3. Fazemos o pedido com o payload
+            response = client.post("/trips/search", json=payload)
         
         # 4. Validamos se o status é 200 e se o JSON tem a estrutura nova
-        assert response.status_code == 200
-        assert response.json()["destination_coordinates"] == {"lat": 48.8534, "lon": 2.3483}
+    assert response.status_code == 200
+    data = response.json()
+    assert data["destination_coordinates"] == fake_coords
+    assert data["weather"] == fake_weather
+    assert data["trip_details"]["destination"] == "Paris"
 
 
 def test_search_trip_invalid_dates():
